9871fa91854dc95fde78486ed53a68f0a5a0eac1
hunterf2301 <36552537+hunterf2301@users.noreply.github.com>
2018-02-16 17:24:14 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_Hunter_Frank.py
@@ -0,0 +1,9 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Name-Hunter Frank
+# Date2-16-18
+#Get the projected sales
+
+total_sales=float(input("Enter total sales:"))
+profit=total_sales*0.23
+print("The profit is $",format(profit," .2f")) 
